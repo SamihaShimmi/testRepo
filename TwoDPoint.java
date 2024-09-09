f4aea12a3a50053c42e3216c30ceef1aa0a44b91
SamihaShimmi <37711125+SamihaShimmi@users.noreply.github.com>
2024-09-09 18:24:14 -0500

    Add files via upload

--- TwoDPoint.java
+++ TwoDPoint.java
@@ -6,7 +6,37 @@ package Shimmi;
 
 // this class accpets two x and y corordinates and generates 2D point using that x and y value
 public class TwoDPoint
-{
+{ void bubbleSort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n - 1; i++) 
+            for (int j = 0; j < n - i - 1; j++) 
+                if (arr[j] > arr[j + 1]) { 
+                    // swap temp and arr[i] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j + 1]; 
+                    arr[j + 1] = temp; 
+                } 
+    } 
+  
+    // Prints the array 
+    void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+        System.out.println(); 
+    } 
+  
+    // Driver method to test above 
+    public static void main(String args[]) 
+    { 
+        BubbleSort ob = new BubbleSort(); 
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
+        ob.bubbleSort(arr); 
+        System.out.println("Sorted array"); 
+        ob.printArray(arr); 
+    } 
     public int x, y, m, n;
     public TwoDPoint(int x, int y)
     {
