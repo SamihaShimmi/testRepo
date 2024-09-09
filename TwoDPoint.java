b1f0195910b0ac92e41f9e4e5718773032fa6d8f
SamihaShimmi <37711125+SamihaShimmi@users.noreply.github.com>
2024-09-09 18:22:51 -0500

    Add files via upload

--- TwoDPoint.java
+++ TwoDPoint.java
@@ -7,7 +7,7 @@ package Shimmi;
 // this class accpets two x and y corordinates and generates 2D point using that x and y value
 public class TwoDPoint
 {
-    public int x, y;
+    public int x, y, m, n;
     public TwoDPoint(int x, int y)
     {
             this.x = y;
